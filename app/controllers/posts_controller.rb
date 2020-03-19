dd113364fed5e97ae960d471a7d39db7465a42e8
yuraef <18228@inbox.ru>
2020-03-19 22:58:54 +0600

    I redid the controllers index, created the view _posts, changed the controllers of the show, edits, update, destroy. Created a method set_post  in the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,18 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only:[:show,:edit,:update,:destroy ]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
+  def show  
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -20,10 +23,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
+  
   def update
-    @post = Post.find(params[:id])
     if @post.update_attributes(post_params)
       redirect_to @post
     else
@@ -31,13 +33,17 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
   def post_params
     params.require(:post).permit(:title,:summary,:body)
   end
-
-
-
-
 end
