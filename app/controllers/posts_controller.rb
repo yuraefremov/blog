b2aba8f3cc279c112d8572cb0aac9139eb6cfced
yuraef <18228@inbox.ru>
2020-03-19 22:35:40 +0600

    Created edit, update methods. Created controllers and views for them. Displayed all posts in the index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
