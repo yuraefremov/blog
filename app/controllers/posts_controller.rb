f9b1c92d1cdace2bf2b7866be339e3b7f00c7b83
yuraef <18228@inbox.ru>
2020-03-27 13:03:03 +0600

    Created a Category model. Created a Categories controller with methods of index show, new, etc. I registered the route of the category. Created similarly to the methods of views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,8 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post, success: 'Статья успешно создана'
     else
-      render :new, danger: 'Статья не  создана'
+      flash.now[:danger] = 'Статья не  создана'
+      render :new
     end
   end
 
@@ -29,7 +30,8 @@ class PostsController < ApplicationController
     if @post.update_attributes(post_params)
       redirect_to @post, success: 'Статья успешно обновлена'
     else
-      render :edit, danger: 'Статья не обновлена'
+      flash.now[:danger] = 'Статья не  создана'
+      render :edit
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_120859) do
+ActiveRecord::Schema.define(version: 2020_03_27_063057) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string "image"
